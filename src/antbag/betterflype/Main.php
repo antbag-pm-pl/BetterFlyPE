176c2bb01842869c4dd463c66aa20162fa0c1d5d
notant <84870445+antbag-dev@users.noreply.github.com>
2023-08-11 01:34:53 +0500

    Create Main.php

---
+++ src/antbag/betterflype/Main.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace antbag\betterflype;
+
+use pocketmine\plugin\PluginBase;
+use pocketmime\command\Command;
+use pocketmime\command\CommandSender;
+use pocketmine\player\Player;
+
+class Main extends PluginBase {
+
+  public function onEnable(): void {
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+    if(!$sender instanceof Player) {
+      $sender->sendMessage("You must be a player to run this command");
+      return false;
+    }
+    
+    if($sender->hasPermission("fly.command.use")) {
+      $this->toggleFlight($sender);
+    }
+  }
+  
+  public static function toggleFlight(Player $player, bool $forceOff = false): void
+      {
+          if ($forceOff) {
+              $player->setAllowFlight(false);
+              $player->setFlying(false);
+              $player->resetFallDistance(); 
+              $player->sendMessage("You can no longer fly.");
+          } else {
+              if (!$player->getAllowFlight()) {
+                  $player->setAllowFlight(true);
+                  $player->sendMessage("You can fly now");    
+              } else {
+                  $player->setAllowFlight(false);
+                  $player->setFlying(false);
+                  $player->resetFallDistance(); 
+                  $player->sendMessage("Fly Disabled");            
+              }
+          }
+      }
+  
+}
